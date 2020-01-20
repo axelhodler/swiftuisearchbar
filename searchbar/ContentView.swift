fd79c07f32bf403ae3b928bb71a5ad1427f238f4
Axel Hodler <axel@hodler.co>
2020-01-20 13:56:26 +0100

    Initial Commit

---
+++ searchbar/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  searchbar
+//
+//  Created by DevPool04 on 20.01.20.
+//  Copyright © 2020 Axel Hodler. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
