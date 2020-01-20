1fd9136abc8de7819cec688d921eb7f1fa541ce5
Axel Hodler <axel@hodler.co>
2020-01-20 16:14:53 +0100

    show minimal searchbar

--- searchbar/ContentView.swift
+++ searchbar/ContentView.swift
@@ -2,15 +2,53 @@ import SwiftUI
 
 struct ContentView: View {
     let cars = ["Subaru WRX", "Tesla Model 3", "Porsche 911", "Renault Zoe", "DeLorean"]
+    @State private var searchTerm : String = ""
+
 
     var body: some View {
         NavigationView {
-            List {
-                ForEach(self.cars, id: \.self) { car in
-                    Text(car)
-                }
-            }.navigationBarTitle(Text("Cars"))
+            VStack {
+                SearchBar(text: $searchTerm)
+                List {
+                    ForEach(self.cars, id: \.self) { car in
+                        Text(car)
+                    }
+                }.navigationBarTitle(Text("Cars"))
+            }
         }
+    }
+}
+
+struct SearchBar: UIViewRepresentable {
+
+    @Binding var text: String
+
+    class Coordinator: NSObject, UISearchBarDelegate {
+
+        @Binding var text: String
+
+        init(text: Binding<String>) {
+            _text = text
+        }
+
+        func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+            text = searchText
+        }
+    }
+
+    func makeCoordinator() -> SearchBar.Coordinator {
+        return Coordinator(text: $text)
+    }
+
+    func makeUIView(context: UIViewRepresentableContext<SearchBar>) -> UISearchBar {
+        let searchBar = UISearchBar(frame: .zero)
+        searchBar.delegate = context.coordinator
+        searchBar.searchBarStyle = .minimal
+        return searchBar
+    }
+
+    func updateUIView(_ uiView: UISearchBar, context: UIViewRepresentableContext<SearchBar>) {
+        uiView.text = text
     }
 }
 
